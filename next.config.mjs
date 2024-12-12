5fe9a87b87e575eaa741d98133aee5a66c8bde76
youcefB95 <belhadri.youcef@yahoo.fr>
2024-12-12 17:04:46 +0100

    modify next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import dotenv from "dotenv"; // Utilisation d'import pour dotenv
+dotenv.config(); // Charge les variables d'environnement à partir de .env
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/data-portfolio" : "",
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
